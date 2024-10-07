bac9bf9d934f0aac6e1cb96741ff678fe235a1a1
Patrick Galvin <patjoegalvin@gmail.com>
2024-10-07 12:46:15 +0000

    Change Background Overlay

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j8c%st4v_vz3qn!b=&5hod#uqp28*v)s26vqi*e4vhhg5+$7@('
+SECRET_KEY = 'django-insecure-j8c%st4v_vz3qn!b=&5hod#uqp28*v)s26vqi*e4vhhg5+€7@('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
